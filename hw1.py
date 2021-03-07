82860cef842a003eb111dfc36358113b2a5ab1e5
108061107 <79581724+108061107@users.noreply.github.com>
2021-03-07 00:04:12 -0800

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -40,17 +40,21 @@ with open(cwb_filename) as csvfile:
 
 #=======================================
 
-num_C0A880 = 0
-num_C0F9A0 = 0
-num_C0G640 = 0
-num_C0R190 = 0
-num_C0X260 = 0
-tot_C0A880 = 0
-tot_C0F9A0 = 0
-tot_C0G640 = 0
-tot_C0R190 = 0
-tot_C0X260 = 0
+num_C0A880 = 0  # data amount of station C0A880
+num_C0F9A0 = 0  # data amount of station C0F9A0
+num_C0G640 = 0  # data amount of station C0G640
+num_C0R190 = 0  # data amount of station C0R190
+num_C0X260 = 0  # data amount of station C0X260
+tot_C0A880 = 0  # preasure sum of station C0A880
+tot_C0F9A0 = 0  # preasure sum of station C0F9A0
+tot_C0G640 = 0  # preasure sum of station C0G640
+tot_C0R190 = 0  # preasure sum of station C0R190
+tot_C0X260 = 0  # preasure sum of station C0X260
 
+# collect "preasure" data of station C0A880, C0F9A0, C0G640, C0R190,and C0X260
+# data not including the -99 and -999 ones
+# sum the preasure data for each station
+# calculate data amount of each station
 for i in range(len(data)):
    element = data[i]
    if element['station_id'] == 'C0A880' and element['PRES'] != '-99.000' and element['PRES'] != '-999.000':
@@ -69,6 +73,8 @@ for i in range(len(data)):
       num_C0X260 = num_C0X260 + 1
       tot_C0X260 = tot_C0X260 + float(element['PRES'])
 
+# construct a list for each station
+# each list contains its station_id and the average of its preasure
 if num_C0A880 != 0:
    data_C0A880 = ['C0A880', tot_C0A880 / num_C0A880]
 else :
@@ -90,6 +96,7 @@ if num_C0X260 != 0:
 else :
    data_C0X260 = ['C0X260', 'none']
 
+# construct a list containing lists of the five stations
 target_data = [data_C0A880, data_C0F9A0, data_C0G640, data_C0R190, data_C0X260]
 
 
